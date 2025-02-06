e2e0677f5b36adec58e766ce8621af91dc21e3e3
FlaviaRamosdaSilva <didi.flavia@gmail.com>
2025-02-06 13:20:38 -0300

    conserto tamanho tela

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import RoutesApp from './routes/routes'
 
 function App() {
+  const location = useLocation()
+
   useEffect(() => {
-    document.documentElement.style.setProperty('zoom', '0.8')
-  }, [])
+    // Verifica se a rota é de login ou esqueceu a senha ou reset da senha
+    if (
+      location.pathname === '/login' ||
+      location.pathname === '/esqueceu-senha' ||
+      location.pathname.startsWith('/users/reset-password/:recoverToken')
+    ) {
+      document.documentElement.style.setProperty('zoom', '1') // 100% para login e esqueceu a senha ou reset da senha
+    } else {
+      document.documentElement.style.setProperty('zoom', '0.8') // 80% para o restante
+    }
+  }, [location.pathname]) // Atualiza quando a rota mudar
   return (
     <div className="App">
       <RoutesApp />
